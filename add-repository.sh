769813f55989369a38f51468a00f41e23d9aadbf
Rodrigo Arce <albo@pragmore.com>
2021-11-13 01:58:56 -0300

    Add update hook

--- add-repository.sh
+++ add-repository.sh
@@ -5,11 +5,16 @@ DOMAIN=$2
 REPO_DIR="/repository/$NAME.git"
 POST_UPDATE_FILE="$REPO_DIR/hooks/post-update"
 
-echo "create repository folder"
-mkdir $REPO_DIR
-cd $REPO_DIR
-git init --bare
-echo "create post update hook"
+if [ ! -d "$REPO_DIR" ]; then
+    echo "create repository folder"
+    mkdir $REPO_DIR
+    cd $REPO_DIR
+    git init --bare
+    echo "create post update hook"
+else
+    echo "update post update hook"
+fi
+
 
 echo "#!/bin/sh" > $POST_UPDATE_FILE
 echo "hook-post-update \"$DOMAIN\" `pwd`" > $POST_UPDATE_FILE
