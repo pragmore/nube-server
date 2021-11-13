058a387f12828737fbeff0440981e1aa4bed2477
Rodrigo Arce <albo@pragmore.com>
2021-11-13 02:15:19 -0300

    Fix bug

--- add-repository.sh
+++ add-repository.sh
@@ -17,5 +17,5 @@ fi
 
 
 echo "#!/bin/sh" > $POST_UPDATE_FILE
-echo "hook-post-update \"$DOMAIN\" `pwd`" > $POST_UPDATE_FILE
+echo "hook-post-update \"$DOMAIN\" \"$REPO_DIR\"" > $POST_UPDATE_FILE
 chmod a+x $POST_UPDATE_FILE
