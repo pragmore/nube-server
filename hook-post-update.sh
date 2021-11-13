ed00100a325ed88f83d45beabe4f5daac5d3edb5
Rodrigo Arce <albo@pragmore.com>
2021-11-13 00:04:15 -0300

    Fix path

--- hook-post-update.sh
+++ hook-post-update.sh
@@ -3,11 +3,12 @@
 HPU_DOMAIN="$1"
 HPU_GIT_DIR="$2"
 HPU_GIT_BRANCH=main
+HPU_PATH="/var/www/$HPU_DOMAIN"
 
 echo -e "\e[1;36mdeploymy.site\e[0m 🚀 \e[34;4mhttps://$HPU_DOMAIN/\e[0m"
-git --work-tree=$HPU_DOMAIN --git-dir=$HPU_GIT_DIR checkout -f -q $HPU_GIT_BRANCH 
+git --work-tree=$HPU_PATH --git-dir=$HPU_GIT_DIR checkout -f -q $HPU_GIT_BRANCH 
 
-cd "/var/www/$HPU_DOMAIN" 
+cd $HPU_PATH
 
 [[ -e composer.json ]] && echo -e -n "Running composer... " && composer install -n -q && echo -e "\e[1;32mok\e[0m"
 
