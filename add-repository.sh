2e796f2fa134a2b6ccb0d3944a9994ff14562960
Rodrigo Arce <albo@pragmore.com>
2021-11-13 01:55:41 -0300

    Remove post-update file

--- add-repository.sh
+++ add-repository.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 NAME=$1
 DOMAIN=$2
-REPLACE_DOMAIN="REPLACE_DOMAIN"
-REPLACE_NAME="REPLACE_NAME"
 
 REPO_DIR="/repository/$NAME.git"
 POST_UPDATE_FILE="$REPO_DIR/hooks/post-update"
@@ -12,6 +10,6 @@ mkdir $REPO_DIR
 cd $REPO_DIR
 git init --bare
 echo "create post update hook"
-cp /repository/post-update $POST_UPDATE_FILE
-sudo sed -i "s/$REPLACE_DOMAIN/$DOMAIN/g" $POST_UPDATE_FILE 
-sudo sed -i "s/$REPLACE_NAME/$NAME/g" $POST_UPDATE_FILE 
+
+echo "#!/bin/sh" > $POST_UPDATE_FILE
+echo "hook-post-update \"$DOMAIN\" `pwd`" > $POST_UPDATE_FILE
