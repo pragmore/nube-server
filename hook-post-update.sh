434520e7e1a5197800922afb6d9a148233cc1fef
Rodrigo Arce <albo@pragmore.com>
2021-11-13 12:52:51 -0300

    Messages changes

--- hook-post-update.sh
+++ hook-post-update.sh
@@ -8,30 +8,40 @@ HPU_PATH="/var/www/$HPU_DOMAIN"
 HPU_OK_MESSAGE="\e[1;32mok\e[0m"
 HPU_ERROR_MESSAGE="\e[1;31merror\e[0m"
 
-echo -e "\e[1m\e[1;36mnube.pragmore.com\e[0m\e[0m 🚀"
+HPU_UPLOAD_MESSAGE="Subir a $HPU_DOMAIN"
+HPU_COPY_CONFIG_MESSAGE="Copiar configuracion inicial... "
+HPU_RUNNING_COMPOSER_MESSAGE="Instalando composer... " 
+HPU_FRAMEWORK_MESSAGE="Framework encontrado: " 
+HPU_MIGRATIONS_MESSAGES="Corriendo migraciones... " 
+HPU_CACHE_CONFIG_MESSAGE="Cacheando configuracion..."
+HPU_CACHE_ROUTES_MESSAGE="Cacheando rutas..."
+HPU_CACHE_VIEWS_MESSAGE="Cacheando vistas..."
+HPU_DONE_MESSAGE="Listo"
+
+echo -e "\e[1m\e[1;36mnube.pragmore.com\e[0m\e[0m 🚀 $HPU_UPLOAD_MESSAGE"
 
 git --work-tree=$HPU_PATH --git-dir=$HPU_GIT_DIR checkout -f -q $HPU_GIT_BRANCH 
 
 cd $HPU_PATH
 
 # Common .env configs
-[ ! -f .env ] && [ -f .env.example ] && echo -e -n "Copy default config... " \
+[ ! -f .env ] && [ -f .env.example ] && echo -e -n $HPU_COPY_CONFIG_MESSAGE \
     && (cp .env.example .env && echo -e $HPU_OK_MESSAGE || echo -e $HPU_ERROR_MESSAGE)
 
 # Common composer install
-[ -f composer.json ] && echo -e -n "Running composer... " \
+[ -f composer.json ] && echo -e -n $HPU_RUNNING_COMPOSER_MESSAGE \
     && (composer install --optimize-autoloader --no-dev -n -q && echo -e $HPU_OK_MESSAGE  || echo -e $HPU_ERROR_MESSAGE)
 
 # Laravel
 if [ -f artisan ]; then 
 
-    echo -e "Framework found... \e[1mLaravel\e[0m"
+    echo -e "$HPU_FRAMEWORK_MESSAGE \e[1mLaravel\e[0m"
 
-    echo -e -n "Cache config... " && php artisan config:cache -q && echo -e $HPU_OK_MESSAGE  || echo -e $HPU_ERROR_MESSAGE
-    echo -e -n "Cache routes... " && php artisan route:cache -q && echo -e $HPU_OK_MESSAGE  || echo -e $HPU_ERROR_MESSAGE
-    echo -e -n "Cache views... " && php artisan view:cache -q && echo -e $HPU_OK_MESSAGE  || echo -e $HPU_ERROR_MESSAGE
-    echo -e -n "Running migrations... " && php artisan migrate --force
+    echo -e -n $HPU_CACHE_CONFIG_MESSAGE && php artisan config:cache -q && echo -e $HPU_OK_MESSAGE  || echo -e $HPU_ERROR_MESSAGE
+    echo -e -n $HPU_CACHE_ROUTES_MESSAGE && php artisan route:cache -q && echo -e $HPU_OK_MESSAGE  || echo -e $HPU_ERROR_MESSAGE
+    echo -e -n $HPU_CACHE_VIEWS_MESSAGE && php artisan view:cache -q && echo -e $HPU_OK_MESSAGE  || echo -e $HPU_ERROR_MESSAGE
+    echo -e -n $HPU_MIGRATIONS_MESSAGES && php artisan migrate --force
 
 fi
 
-echo -e "\e[1;32mDone\e[0m ✅ → 🔗 \e[34;4mhttps://$HPU_DOMAIN/\e[0m"
+echo -e "\e[1;32m$HPU_DONE_MESSAGE\e[0m → 🔗 \e[34;4mhttps://$HPU_DOMAIN/\e[0m"
