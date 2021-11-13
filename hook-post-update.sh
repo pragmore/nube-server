ebcecb840684e1792fdb1775b79002e21441a6d6
Rodrigo Arce <albo@pragmore.com>
2021-11-12 23:52:33 -0300

    Remove debug on hook

--- hook-post-update.sh
+++ hook-post-update.sh
@@ -4,14 +4,13 @@ HPU_DOMAIN=$1
 HPU_GIT_DIR=$2
 HPU_GIT_BRANCH=main
 
-echo -e "DOMAIN: $HPU_DOMAIN"
-echo -e "HPU_GIT_DIR $HPU_GIT_DIR"
-
-echo -e "\e[1;36mdeploymy.site\e[0m <U+1F680> \e[34;4mhttps://$HPU_DOMAIN/\e[0m"
+echo -e "\e[1;36mdeploymy.site\e[0m 🚀 \e[34;4mhttps://$HPU_DOMAIN/\e[0m"
 git --work-tree=$HPU_DOMAIN --git-dir=$HPU_GIT_DIR checkout -f -q $HPU_GIT_BRANCH 
+
 cd "/var/www/$HPU_DOMAIN" 
-pwd
+
 [[ -e composer.json ]] && echo -e -n "Running composer... " && composer install -n -q && echo -e "\e[1;32mok\e[0m"
+
 echo -e -n "Running migrations... "
 echo -e "\e[2mno migrations\e[0m"
 echo -e -n "Running config... "
