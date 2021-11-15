7843e9a92b051f2db3932339d12ca3cbcbf44eaa
Rodrigo Arce <albo@pragmore.com>
2021-11-15 11:16:56 -0300

    Create db fix bug

--- hook-post-update.sh
+++ hook-post-update.sh
@@ -31,7 +31,7 @@ echo -e "\e[36;1mnube.pragmore.com\e[0m 🚀 $HPU_UPLOAD_MESSAGE"
 
 git --work-tree=$HPU_PATH --git-dir=$HPU_GIT_DIR checkout -f -q $HPU_GIT_BRANCH 
 
-echo "CREATE DATABASE IF NOT EXISTS $HBU_SLUG" | \
+echo "CREATE DATABASE IF NOT EXISTS $HPU_SLUG" | \
     mysql -u$HPU_DB_USER -p$HPU_DB_PASS --host=$HPU_DB_HOST --port=$HPU_DB_PORT
 
 cd $HPU_PATH
