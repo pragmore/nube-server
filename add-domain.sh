c71c5c9ca7ac71e6edd6bfca060bc7a9aea2bb18
Rodrigo Arce <albo@pragmore.com>
2021-11-19 23:42:29 -0300

    Fix add domain

--- add-domain.sh
+++ add-domain.sh
@@ -10,4 +10,4 @@ mkdir -p $PUBLIC_PATH
 ln -s $WEB_PATH "/var/www/$DOMAIN"
 echo "creating home page"
 echo "<html><body><?= \"$DOMAIN \" ?></body></html>" > "$PUBLIC_PATH/index.php"
-/home/ec2-user/.acme.sh/acme.sh --issue -d $DOMAIN -w "$PUBLIC_PATH --stateless"
+/home/ec2-user/.acme.sh/acme.sh --issue -d $DOMAIN -w "$PUBLIC_PATH" --stateless
