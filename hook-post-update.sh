79eb720f6deee8aabb53a93ef7bbe405fe52d152
Rodrigo “Albo” Arce <albo@pragmore.com>
2023-04-24 01:40:28 -0300

    npm install only production

--- hook-post-update.sh
+++ hook-post-update.sh
@@ -82,7 +82,7 @@ composer_install() {
 
 npm_install() {
   [ -f package.json ] && echo -e $NUBE_RUNNING_NPM_MESSAGE \
-    && (NODE_OPTIONS=--max-old-space-size=950 npm ci \
+    && (NODE_OPTIONS=--max-old-space-size=950 npm ci --omit=dev \
         && echo -e $NUBE_OK_MESSAGE  || echo -e $NUBE_ERROR_MESSAGE)
 }
 
