6eed0bb4d18f5b304a5c9071f921634e83061be2
Rodrigo “Albo” Arce <albo@pragmore.com>
2023-04-27 23:29:04 -0300

    Show only message of restarting PM2 when exists

--- hook-post-update.sh
+++ hook-post-update.sh
@@ -111,9 +111,9 @@ npm_install() {
 }
 
 pm2_restart() {
-  [ -f package.json ] && echo -e $NUBE_RESTARTING_PM2_MESSAGE \
-    && (pm2 ls | grep  $NUBE_DOMAIN \
-        && (pm2 restart $NUBE_DOMAIN \
+  [ -f package.json ] && (pm2 ls | grep  $NUBE_DOMAIN \
+        && (echo -e $NUBE_RESTARTING_PM2_MESSAGE \
+          && pm2 restart $NUBE_DOMAIN \
           && echo -e $NUBE_OK_MESSAGE  || nube_error))
 }
 
